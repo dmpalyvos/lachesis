a3b133a0ea83c3b0bfdbe79136224b1b47922900
Dimitris Palyvos <palyvos@chalmers.se>
2021-02-18 16:03:51 +0100

    Automatically adjust StormGraphiteMetric queries to multi-part hostnames.

--- src/main/java/io/palyvos/scheduler/adapters/storm/StormGraphiteMetric.java
+++ src/main/java/io/palyvos/scheduler/adapters/storm/StormGraphiteMetric.java
@@ -2,18 +2,32 @@ package io.palyvos.scheduler.adapters.storm;
 
 import io.palyvos.scheduler.metric.Metric;
 import io.palyvos.scheduler.util.SchedulerContext;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Collections;
 import java.util.Map;
 
 public enum StormGraphiteMetric implements Metric<StormGraphiteMetric> {
   TASK_QUEUE_SIZE_FROM_SUBTASK_DATA(
-      "groupByNode(Storm.*.*.*.*.*.receive.population.value, 4, 'sum')"),
-  SUBTASK_TUPLES_IN_RECENT("groupByNode(Storm.*.*.*.*.*.execute-count.*.value, 4, 'sum')"),
-  SUBTASK_TUPLES_OUT_RECENT("groupByNode(Storm.*.*.*.*.*.transfer-count.*.value, 4, 'sum')");
+      "groupByNode(Storm.*.%s.*.*.*.receive.population.value, %d, 'sum')"),
+  SUBTASK_TUPLES_IN_RECENT("groupByNode(Storm.*.%s.*.*.*.execute-count.*.value, %d, 'sum')"),
+  SUBTASK_TUPLES_OUT_RECENT("groupByNode(Storm.*.%s.*.*.*.transfer-count.*.value, %d, 'sum')");
 
   private final String graphiteQuery;
+  private final int operatorBaseIndex = 3;
 
   StormGraphiteMetric(String graphiteQuery) {
-    this.graphiteQuery = graphiteQuery;
+    //query format: Storm.jobName.[hostname-part]+.worker.node.instance...
+    try {
+      int hostnamePartsNumber = InetAddress.getLocalHost().getCanonicalHostName()
+          .split("\\.").length;
+      String hostnamePattern = String.join(".", Collections.nCopies(hostnamePartsNumber, "*"));
+      this.graphiteQuery = String
+          .format(graphiteQuery, hostnamePattern, operatorBaseIndex + hostnamePartsNumber);
+    } catch (UnknownHostException e) {
+      throw new IllegalStateException(
+          String.format("Hostname not defined correctly in this machine: %s", e.getMessage()));
+    }
   }
 
 
