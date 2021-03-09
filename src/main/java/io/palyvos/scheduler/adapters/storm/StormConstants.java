ae81e1824569c79b82baa841ceb100fbe08ade5b
Dimitris Palyvos <palyvos@chalmers.se>
2021-03-09 16:29:49 +0100

    Detect disruptor queue helper threads in Storm executions.

--- src/main/java/io/palyvos/scheduler/adapters/storm/StormConstants.java
+++ src/main/java/io/palyvos/scheduler/adapters/storm/StormConstants.java
@@ -3,13 +3,17 @@ package io.palyvos.scheduler.adapters.storm;
 import java.util.regex.Pattern;
 
 public class StormConstants {
+
   public static final String DEFAULT_HOST = "localhost";
   public static final int DEFAULT_PORT = 8080;
   public static final String STORM_WORKER_CLASS = "org.apache.storm.daemon.worker";
   public static final Pattern EXECUTOR_THREAD_PATTERN = Pattern
-      .compile("Thread-\\d+-(.+)-executor\\[\\d+ \\d+\\]");
+      .compile("Thread-\\d+-(.+)-executor\\[(\\d+) \\d+\\]");
+  public static final Pattern DISRUPTOR_THREAD_PATTERN = Pattern
+      .compile("Thread-\\d+-disruptor-executor\\[(\\d+) \\d+\\]-send-queue");
   public static final String ACKER_NAME = "_acker";
   public static final String METRIC_REPORTER_NAME = "MetricReporter";
+
   private StormConstants() {
   }
 }
--- src/main/java/io/palyvos/scheduler/adapters/storm/StormThreadAssigner.java
+++ src/main/java/io/palyvos/scheduler/adapters/storm/StormThreadAssigner.java
@@ -1,6 +1,7 @@
 package io.palyvos.scheduler.adapters.storm;
 
 import io.palyvos.scheduler.task.ExternalThread;
+import io.palyvos.scheduler.task.HelperTask;
 import io.palyvos.scheduler.task.Subtask;
 import io.palyvos.scheduler.task.Task;
 import java.util.Collection;
@@ -8,15 +9,18 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.regex.Matcher;
 
+//Thread-33-disruptor-executor[11 11]-send-queue
 class StormThreadAssigner {
 
   public static void assign(Collection<Task> tasks, Collection<ExternalThread> threads) {
     final Map<String, Task> taskIndex = new HashMap<>();
+    final Map<Integer, Task> executorsIndex = new HashMap<>();
     tasks.forEach(task -> taskIndex.put(task.id(), task));
     for (ExternalThread thread : threads) {
       Matcher matcher = StormConstants.EXECUTOR_THREAD_PATTERN.matcher(thread.name());
       if (matcher.matches()) {
         final String taskId = matcher.group(1);
+        final int executorId = Integer.valueOf(matcher.group(2));
         if (taskId.contains(StormConstants.ACKER_NAME) || taskId.contains(
             StormConstants.METRIC_REPORTER_NAME)) {
           continue;
@@ -27,6 +31,17 @@ class StormThreadAssigner {
         Subtask subtask = new Subtask(taskId, taskId, task.subtasks().size());
         subtask.assignThread(thread);
         task.subtasks().add(subtask);
+        executorsIndex.put(executorId, task);
+      }
+    }
+    for (ExternalThread thread : threads) {
+      Matcher matcher = StormConstants.DISRUPTOR_THREAD_PATTERN.matcher(thread.name());
+      if (matcher.matches()) {
+        final int executorId = Integer.valueOf(matcher.group(1));
+        Task mainTask = executorsIndex.get(executorId);
+        if (mainTask != null) {
+          mainTask.helpers().add(new HelperTask(thread));
+        }
       }
     }
   }
